888b8025fca3bc424b6318e20077acda51219a25
Daniel Zambon <danielfzambon@gmail.com>
2022-04-28 22:58:31 -0300

    Comunicacao RestTemplate refatorado

---
+++ hr-payroll/src/main/java/com/danielzambon/hrpayroll/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.danielzambon.hrpayroll.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public RestTemplate resTemplate() {
+		return new RestTemplate();
+	}
+
+}

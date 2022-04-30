77941a4b3971812a7b92ba422dc8fd7f66977b82
Daniel Zambon <danielfzambon@gmail.com>
2022-04-29 23:24:29 -0300

    Rest para Feign Clients

--- hr-payroll/src/main/java/com/danielzambon/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/danielzambon/hrpayroll/services/PaymentService.java
@@ -1,15 +1,23 @@
 package com.danielzambon.hrpayroll.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.danielzambon.hrpayroll.entities.Payment;
+import com.danielzambon.hrpayroll.entities.Worker;
+import com.danielzambon.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
+
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+
 	
 	public Payment getPayment(Long workerId, int days) {
 		
-		return new Payment("Bob",200.0,days);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 		
 	}
 
